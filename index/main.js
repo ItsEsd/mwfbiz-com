9c511546db76b37acb50d14330eb8e8c315444d7
soubhikdas <d.soubhik@hotmail.com>
2024-03-03 14:40:54 +0530

    to base64

--- index/main.js
+++ index/main.js
@@ -1,13 +1,14 @@
 /* mwfbiz.COM  */
-$(document).ready(function(){
-    document.getElementById('premiumpromo').innerHTML=" <p>👑 BIZ Admin (OBDM): <a href='https://mwfbiz.com/feedback'>Contact Desk</a> ** Admin Trial - 7200/- 👑</p>";
-    });
-
-$('#promodiv_l').click(function(){
-    $('#promodiv').slideUp('fast');
-    document.getElementById('promodiv').style.height = '0px';
+$(document).ready(function () {
+  document.getElementById("premiumpromo").innerHTML =
+    " <p>&#128081; BIZ Admin (OBDM): <a href='https://mwfbiz.com/feedback'>Contact Desk</a> ** Admin Trial - 7200/- &#128081;</p>";
 });
-$('#showpromodiv').click(function(){
-    $('#promodiv').slideDown('fast');
-    document.getElementById('promodiv').style.height = '100%';
+
+$("#promodiv_l").click(function () {
+  $("#promodiv").slideUp("fast");
+  document.getElementById("promodiv").style.height = "0px";
+});
+$("#showpromodiv").click(function () {
+  $("#promodiv").slideDown("fast");
+  document.getElementById("promodiv").style.height = "100%";
 });
